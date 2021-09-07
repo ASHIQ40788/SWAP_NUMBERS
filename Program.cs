6d7d6cdf8a600753f3b5f76d3005076a6c46b84d
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-07 19:51:58 +0530

    [ASHIQ] added: SWAPPING OF TWO NUMBERS

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SWAP_TWO_NUMBERS
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int number1 = 10, number2 = 20, temp = 0;
+            Console.WriteLine($"Before SWapping number1= {number1}, number2 = {number2}");
+            temp = number1; //temp=10
+            number1 = number2; //number1=20      
+            number2 = temp; //number2=10    
+            Console.WriteLine($"After swapping number1= {number1}, number2 = {number2}");
+            Console.ReadKey();
+        }
+    }
+}
